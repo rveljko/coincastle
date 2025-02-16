9cbed3d2888139979c85bc29ca50a14134d947ae
Veljko <radisavljevicveljko123@gmail.com>
2025-02-16 17:38:33 +0100

    add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,15 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
