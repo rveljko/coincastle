a734feaed86e10a2e53b944ffcdf4de9d9333109
Veljko <radisavljevicveljko123@gmail.com>
2025-02-23 12:22:32 +0100

    change theme images to png format

--- src/data/themes.ts
+++ src/data/themes.ts
@@ -6,18 +6,18 @@ export const themes: Theme[] = [
     id: 1,
     name: 'System Preferences',
     label: 'system',
-    imageUrl: `${IMAGE_PATH}theme-system.svg`,
+    imageUrl: `${IMAGE_PATH}theme-system.png`,
   },
   {
     id: 2,
     name: 'Dark',
     label: 'dark',
-    imageUrl: `${IMAGE_PATH}theme-dark.svg`,
+    imageUrl: `${IMAGE_PATH}theme-dark.png`,
   },
   {
     id: 3,
     name: 'Light',
     label: 'light',
-    imageUrl: `${IMAGE_PATH}theme-light.svg`,
+    imageUrl: `${IMAGE_PATH}theme-light.png`,
   },
 ]
