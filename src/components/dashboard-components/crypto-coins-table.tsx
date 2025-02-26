4be6ac58587842eac4f651624d943812b56743c2
Veljko <radisavljevicveljko123@gmail.com>
2025-02-26 18:25:59 +0100

    make crypto coins table rows clickable

--- src/components/dashboard-components/crypto-coins-table.tsx
+++ src/components/dashboard-components/crypto-coins-table.tsx
@@ -7,6 +7,7 @@ import {
   compactCurrencyFormatter,
   currencyFormatter,
 } from '@utils/helpers/currency-formatter'
+import { Link, useNavigate } from 'react-router-dom'
 
 type CryptoCoinsTableProps = {
   numberOfCoins: number
@@ -16,6 +17,7 @@ export default function CryptoCoinsTable({
   numberOfCoins,
 }: CryptoCoinsTableProps) {
   const { data, isPending, error } = useGetCryptoCurrencies(numberOfCoins)
+  const navigate = useNavigate()
 
   if (isPending)
     return <CryptoCoinsTableSkeleton numberOfCoins={numberOfCoins} />
@@ -44,18 +46,30 @@ export default function CryptoCoinsTable({
             market_cap_rank,
             total_volume,
           }) => (
-            <Table.BodyRow key={id}>
+            <Table.BodyRow
+              key={id}
+              onClick={(e) => {
+                if (e.metaKey || e.ctrlKey) {
+                  window.open(`/dashboard/coin/${id}`, '_blank')
+                } else {
+                  navigate(`/dashboard/coin/${id}`)
+                }
+              }}
+              className="cursor-pointer"
+            >
               <Table.BodyCell>{market_cap_rank}</Table.BodyCell>
               <Table.BodyCell>
-                <div className="flex w-max flex-row gap-1">
-                  <img
-                    className="size-5.5 rounded-full"
-                    src={image}
-                    title={name}
-                    alt={name}
-                  />
-                  {name}
-                </div>
+                <Link to={`/dashboard/coin/${id}`}>
+                  <div className="flex w-max flex-row gap-1">
+                    <img
+                      className="size-5.5 rounded-full"
+                      src={image}
+                      title={name}
+                      alt={name}
+                    />
+                    {name}
+                  </div>
+                </Link>
               </Table.BodyCell>
               <Table.BodyCell>
                 {currencyFormatter(current_price)}
