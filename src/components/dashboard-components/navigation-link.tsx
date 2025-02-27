db8dcad2d6e6436e0d72c6d616267ed7204b5f22
Veljko <radisavljevicveljko123@gmail.com>
2025-02-27 12:51:09 +0100

    remove false classname from inactive navigation link

--- src/components/dashboard-components/navigation-link.tsx
+++ src/components/dashboard-components/navigation-link.tsx
@@ -16,8 +16,9 @@ export default function NavigationLink({
     <NavLink
       className={({ isActive }) =>
         `flex flex-row gap-1 rounded-sm p-0.5 text-neutral-400 hover:bg-neutral-600 hover:bg-[linear-gradient(hsl(from_var(--color-white)_h_s_l_/_20%),_transparent)] hover:text-white hover:inset-ring-1 hover:inset-ring-neutral-600 ${
-          isActive &&
-          'bg-neutral-700 bg-[linear-gradient(hsl(from_var(--color-white)_h_s_l_/_20%),_transparent)] text-white inset-ring-1 inset-ring-neutral-700'
+          isActive
+            ? 'bg-neutral-700 bg-[linear-gradient(hsl(from_var(--color-white)_h_s_l_/_20%),_transparent)] text-white inset-ring-1 inset-ring-neutral-700'
+            : ''
         }`
       }
       end
