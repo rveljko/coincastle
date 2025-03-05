98247c5af3a05d1774f084cdef1c362baefd20f4
Veljko <radisavljevicveljko123@gmail.com>
2025-03-05 10:37:35 +0100

    refactor nft collections table skeleton

--- src/components/dashboard-components/nft-collections-table-skeleton.tsx
+++ src/components/dashboard-components/nft-collections-table-skeleton.tsx
@@ -12,9 +12,6 @@ export default function NftCollectionsTableSkeleton({
       <Table.Header>
         <Table.HeaderRow>
           <Table.HeaderCell>
-            <div className="bg-clickable/10 size-5.5 animate-pulse rounded-sm"></div>
-          </Table.HeaderCell>
-          <Table.HeaderCell>
             <div className="bg-clickable/10 h-5.5 w-20 animate-pulse rounded-sm"></div>
           </Table.HeaderCell>
           <Table.HeaderCell>
@@ -35,9 +32,6 @@ export default function NftCollectionsTableSkeleton({
         {Array.from({ length: numberOfCoins }, (_, index) => index).map(
           (row) => (
             <Table.BodyRow key={row} className="hover:bg-unset">
-              <Table.BodyCell>
-                <div className="bg-clickable/10 size-5.5 animate-pulse rounded-sm"></div>
-              </Table.BodyCell>
               <Table.BodyCell>
                 <div className="flex gap-1">
                   <div className="bg-clickable/10 size-5.5 animate-pulse rounded-sm"></div>
