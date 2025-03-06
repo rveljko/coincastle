96de1813e13b6edb9825bd1d3ddde8cccb9ff4a7
Veljko <radisavljevicveljko123@gmail.com>
2025-03-06 15:29:03 +0100

    remove dashboard section stylings

--- src/sections/dashboard-sections/section.tsx
+++ src/sections/dashboard-sections/section.tsx
@@ -1,19 +1,8 @@
-import { cn } from '@utils/utils'
-
 type SectionProps = {
   children: React.ReactNode
   className?: string
 }
 
 export default function Section({ children, className }: SectionProps) {
-  return (
-    <section
-      className={cn(
-        'border-section-outline w-full rounded-xl border bg-(image:--section-background) bg-fixed p-4',
-        className
-      )}
-    >
-      {children}
-    </section>
-  )
+  return <section className={className}>{children}</section>
 }
