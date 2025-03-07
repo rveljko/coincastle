594361dd77d1f39211302d4812e7d1ade117706e
Veljko <radisavljevicveljko123@gmail.com>
2025-03-07 11:21:06 +0100

    fix container max width

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -8,7 +8,10 @@ type ContainerProps = {
 export default function Container({ children, className }: ContainerProps) {
   return (
     <div
-      className={cn('mx-auto w-full max-w-(--container-width) px-4', className)}
+      className={cn(
+        'mx-auto w-[calc(100%_-_(var(--spacing)_*_8))] max-w-(--container-width)',
+        className
+      )}
     >
       {children}
     </div>
