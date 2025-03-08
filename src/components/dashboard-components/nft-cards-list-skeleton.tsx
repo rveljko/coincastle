dded097a49d697c91bc9a127ce47312a90016567
Veljko <radisavljevicveljko123@gmail.com>
2025-03-08 14:11:48 +0100

    add key to nft cards list skeleton

--- src/components/dashboard-components/nft-cards-list-skeleton.tsx
+++ src/components/dashboard-components/nft-cards-list-skeleton.tsx
@@ -1,8 +1,8 @@
 export default function NftCardsListSkeleton() {
   return (
     <div className="grid grid-cols-[repeat(auto-fit,_minmax(min(calc(var(--spacing)_*_70.25),_100%),_1fr))] gap-4.5">
-      {Array.from({ length: 20 }, (_, index) => index).map(() => (
-        <div>
+      {Array.from({ length: 20 }, (_, index) => index).map((index) => (
+        <div key={index}>
           <div className="bg-clickable/10 mb-2 aspect-1/1 animate-pulse rounded-sm"></div>
           <div className="mb-3 space-y-1">
             <div className="bg-clickable/10 h-4 w-full animate-pulse rounded-sm"></div>
