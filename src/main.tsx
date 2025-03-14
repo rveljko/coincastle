b7739bfbd6255025c0c9a65e617160f17c17e283
Veljko <radisavljevicveljko123@gmail.com>
2025-03-14 12:46:05 +0100

    disable refetching queries on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './assets/index.css'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, 
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
