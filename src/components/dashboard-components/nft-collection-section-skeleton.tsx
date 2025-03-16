603fa2400db0c224f9c7fdc998ce2ebfd81bd8d4
Veljko <radisavljevicveljko123@gmail.com>
2025-03-16 10:25:58 +0100

    move sorting button from nft collection nfts skeleton to nft collection section skeleton

--- src/components/dashboard-components/nft-collection-section-skeleton.tsx
+++ src/components/dashboard-components/nft-collection-section-skeleton.tsx
@@ -20,6 +20,7 @@ export default function NftCollectionSectionSkeleton() {
         </div>
         <NftCollectionStatisticsSkeleton />
       </div>
+      <div className="bg-clickable/10 mb-2 h-9.5 w-20 animate-pulse rounded-sm"></div>
       <NftCollectionNftsSkeleton />
     </Section>
   )
@@ -52,10 +53,5 @@ export function NftCollectionStatisticsSkeleton() {
 }
 
 export function NftCollectionNftsSkeleton() {
-  return (
-    <>
-      <div className="bg-clickable/10 mb-2 h-9.5 w-20 animate-pulse rounded-sm"></div>
-      <NftCardsListSkeleton />
-    </>
-  )
+  return <NftCardsListSkeleton />
 }
