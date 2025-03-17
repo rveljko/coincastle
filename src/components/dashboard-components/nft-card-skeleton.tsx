a25b9f9f1a6ce13225e0a5951bbd5f364b846fa0
Veljko <radisavljevicveljko123@gmail.com>
2025-03-17 13:01:58 +0100

    add price to nft card skeleton

--- src/components/dashboard-components/nft-card-skeleton.tsx
+++ src/components/dashboard-components/nft-card-skeleton.tsx
@@ -2,10 +2,11 @@ export default function NftCardSkeleton() {
   return (
     <div>
       <div className="bg-clickable/10 mb-2 aspect-1/1 animate-pulse rounded-sm"></div>
-      <div className="mb-3 space-y-1">
+      <div className="mb-2 space-y-1">
         <div className="bg-clickable/10 h-4 w-full animate-pulse rounded-sm"></div>
         <div className="bg-clickable/10 h-4 w-[50%] animate-pulse rounded-sm"></div>
       </div>
+      <div className="bg-clickable/10 mb-3 h-5 w-20 animate-pulse rounded-sm"></div>
     </div>
   )
 }
