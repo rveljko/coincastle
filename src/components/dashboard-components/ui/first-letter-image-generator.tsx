5f334b7be537a6f824a6da3414ea31ecae751c5e
Veljko <radisavljevicveljko123@gmail.com>
2025-03-19 19:28:54 +0100

    make first letter image generator unselectable

--- src/components/dashboard-components/ui/first-letter-image-generator.tsx
+++ src/components/dashboard-components/ui/first-letter-image-generator.tsx
@@ -9,7 +9,7 @@ export default function FirstLetterImageGenerator({
 }: FirstLetterImageGeneratorProps) {
   return (
     <div
-      className={`flex items-center justify-center rounded-full bg-neutral-700 ${isSmall ? 'size-5.5' : 'size-8'}`}
+      className={`flex items-center justify-center rounded-full bg-neutral-700 select-none ${isSmall ? 'size-5.5' : 'size-8'}`}
     >
       <span
         className={`text-neutral-100 ${isSmall ? 'text-paragraph-font-size leading-paragraph-line-height' : 'text-heading-4-font-size leading-heading-4-line-height'}`}
