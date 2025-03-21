c06628875eafd88f65fcc6c54e83eeecfe2649f8
Veljko <radisavljevicveljko123@gmail.com>
2025-03-21 09:44:50 +0100

    add error messages for stock api functions

--- src/services/api/stocks.ts
+++ src/services/api/stocks.ts
@@ -11,8 +11,13 @@ export async function getStocks(
   limit: number
 ): Promise<StockOverviewHttpResponse[]> {
   const res = await fetch(
-    `https://financialmodelingprep.com/stable/company-screener?country=us&marketCapMoreThan=1000000000&volumeMoreThan=1000000&limit=${limit}&apikey=${import.meta.env.VITE_FMP_API_KEY}`
+    `https://financialmodelingprep.com/stable/company-screener?country=us&marketCapMoreThan=1000000000&volumeMoreThan=1000000&limit=${limit}apikey=${import.meta.env.VITE_FMP_API_KEY}`
   )
+
+  if (!res.ok)
+    throw new Error(
+      'Failed to fetch all stocks. Please check your connection and try again.'
+    )
 
   return res.json()
 }
@@ -23,6 +28,12 @@ export async function getStockInformation(
   const res = await fetch(
     `https://financialmodelingprep.com/stable/quote?symbol=${symbol}&apikey=${import.meta.env.VITE_FMP_API_KEY}`
   )
+
+  if (!res.ok)
+    throw new Error(
+      'Failed to fetch stock information. Ensure the stock symbol is correct and try again.'
+    )
+
   return res.json()
 }
 
@@ -33,5 +44,11 @@ export async function getStockChartInformation(
   const res = await fetch(
     `https://financialmodelingprep.com/stable/historical-price-eod/light?symbol=${symbol}&from=${iso8601DateFormatter(generateDateInPast(parseInt(period)))}&to=${iso8601DateFormatter(new Date())}&apikey=${import.meta.env.VITE_FMP_API_KEY}`
   )
+
+  if (!res.ok)
+    throw new Error(
+      'Failed to fetch stock chart information. Check the stock symbol and time period, then try again.'
+    )
+
   return res.json()
 }
