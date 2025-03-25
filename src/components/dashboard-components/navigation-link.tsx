9472b46960343c1407e0a083def6bf4aa0063ddf
Veljko <radisavljevicveljko123@gmail.com>
2025-03-25 07:57:26 +0100

    fix navigation link alignment

--- src/components/dashboard-components/navigation-link.tsx
+++ src/components/dashboard-components/navigation-link.tsx
@@ -15,7 +15,7 @@ export default function NavigationLink({
   return (
     <NavLink
       className={({ isActive }) =>
-        `hover:text-clickable flex flex-row gap-1 rounded-sm p-0.5 text-neutral-400 hover:bg-neutral-600 hover:bg-[linear-gradient(hsl(from_var(--color-white)_h_s_l_/_20%),_transparent)] hover:inset-ring-1 hover:inset-ring-neutral-600 ${
+        `hover:text-clickable flex flex-row items-center gap-1 rounded-sm p-0.5 text-neutral-400 hover:bg-neutral-600 hover:bg-[linear-gradient(hsl(from_var(--color-white)_h_s_l_/_20%),_transparent)] hover:inset-ring-1 hover:inset-ring-neutral-600 ${
           isActive
             ? 'text-clickable! bg-neutral-700 bg-[linear-gradient(hsl(from_var(--color-white)_h_s_l_/_20%),_transparent)] inset-ring-1 inset-ring-neutral-700'
             : ''
