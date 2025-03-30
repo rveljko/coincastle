e3fb834156fe817527ef82eca4c744378f4af47c
Veljko <radisavljevicveljko123@gmail.com>
2025-03-30 13:01:42 +0200

    change guides section component to semantic element

--- src/sections/dashboard-sections/guides-section.tsx
+++ src/sections/dashboard-sections/guides-section.tsx
@@ -1,13 +1,12 @@
 import GuideCardsList from '@components/dashboard-components/guide-cards-list'
 import { guides } from '@data/guides'
-import Section from '@sections/dashboard-sections/section'
 
 export default function GuidesSection() {
   return (
-    <Section>
+    <section>
       <h1 className="mb-2">Guides</h1>
       <p className="mb-4">Your favorite place when you are confused</p>
       <GuideCardsList guides={guides} />
-    </Section>
+    </section>
   )
 }
