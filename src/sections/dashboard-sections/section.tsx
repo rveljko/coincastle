c855a716f5a8d3eb4960576e1914c36a54398ede
Veljko <radisavljevicveljko123@gmail.com>
2025-04-03 21:27:13 +0200

    add stylings to dashboard section

--- src/sections/dashboard-sections/section.tsx
+++ src/sections/dashboard-sections/section.tsx
@@ -1,8 +1,19 @@
+import { cn } from '@utils/utils'
+
 type SectionProps = {
   children: React.ReactNode
   className?: string
 }
 
 export default function Section({ children, className }: SectionProps) {
-  return <section className={className}>{children}</section>
+  return (
+    <section
+      className={cn(
+        'border-section-outline relative rounded-xl border p-4 after:absolute after:inset-0 after:-z-1 after:rounded-[calc(var(--spacing)_*_2.75)] after:bg-(image:--section-background) after:bg-fixed',
+        className
+      )}
+    >
+      {children}
+    </section>
+  )
 }
