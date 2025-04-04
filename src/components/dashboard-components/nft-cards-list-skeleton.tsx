734cefe91794eb43ab79afee32c5a4d8644845ca
Veljko <radisavljevicveljko123@gmail.com>
2025-04-04 08:55:28 +0200

    refactor nft cards list skeleton to display 4 cards in a row

--- src/components/dashboard-components/nft-cards-list-skeleton.tsx
+++ src/components/dashboard-components/nft-cards-list-skeleton.tsx
@@ -2,7 +2,7 @@ import NftCardSkeleton from '@components/dashboard-components/nft-card-skeleton'
 
 export default function NftCardsListSkeleton() {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(min(calc(var(--spacing)_*_70.25),_100%),_1fr))] gap-4.5">
+    <div className="grid grid-cols-[repeat(auto-fit,_minmax(min(var(--nft-card-width),_100%),_1fr))] gap-4">
       {Array.from({ length: 20 }, (_, index) => index).map((index) => (
         <NftCardSkeleton key={index} />
       ))}
