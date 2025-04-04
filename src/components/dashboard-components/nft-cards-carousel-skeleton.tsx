6a3c2c961434fa1f71cef8043611a393095229ce
Veljko <radisavljevicveljko123@gmail.com>
2025-04-04 09:00:39 +0200

    change gap in nft cards carousel skeleton

--- src/components/dashboard-components/nft-cards-carousel-skeleton.tsx
+++ src/components/dashboard-components/nft-cards-carousel-skeleton.tsx
@@ -3,7 +3,7 @@ import NftCardSkeleton from '@components/dashboard-components/nft-card-skeleton'
 export default function NftCardsCarouselSkeleton() {
   return (
     <div className="w-0 min-w-full overflow-x-auto">
-      <div className="mb-2 flex gap-2">
+      <div className="mb-2 flex gap-4">
         {Array.from({ length: 10 }, (_, index) => index).map((index) => (
           <div className="min-w-(--nft-card-width)" key={index}>
             <NftCardSkeleton />
