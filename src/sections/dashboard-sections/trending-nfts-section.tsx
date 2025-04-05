dcb4e655fe870b0a8d17c1864e87300a7f7e471b
Veljko <radisavljevicveljko123@gmail.com>
2025-04-05 13:03:02 +0200

    fix trending nfts section error position

--- src/sections/dashboard-sections/trending-nfts-section.tsx
+++ src/sections/dashboard-sections/trending-nfts-section.tsx
@@ -11,15 +11,17 @@ export default function TrendingNftsSection({
 }: TrendingNftsSectionProps) {
   return (
     <Section className={className}>
-      <header className="mb-2 flex flex-wrap items-center justify-between gap-2">
-        <h2 className="text-heading-4-font-size leading-heading-4-line-height">
-          Trending NFTs
-        </h2>
-        <Button href="/dashboard/nfts" variant="link" size="small">
-          View More
-        </Button>
-      </header>
-      <TrendingNftCollectionsTable />
+      <div className="flex h-full flex-col justify-between gap-2">
+        <header className="flex flex-wrap items-center justify-between gap-2">
+          <h2 className="text-heading-4-font-size leading-heading-4-line-height">
+            Trending NFTs
+          </h2>
+          <Button href="/dashboard/nfts" variant="link" size="small">
+            View More
+          </Button>
+        </header>
+        <TrendingNftCollectionsTable />
+      </div>
     </Section>
   )
 }
