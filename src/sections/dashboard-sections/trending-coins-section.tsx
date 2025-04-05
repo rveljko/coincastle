3d03a087a650f0d052cb4505d5dd80d586ab5e4f
Veljko <radisavljevicveljko123@gmail.com>
2025-04-05 13:04:51 +0200

    fix trending coins section error position

--- src/sections/dashboard-sections/trending-coins-section.tsx
+++ src/sections/dashboard-sections/trending-coins-section.tsx
@@ -11,15 +11,17 @@ export default function TrendingCoinsSection({
 }: TrendingCoinsSectionProps) {
   return (
     <Section className={className}>
-      <header className="mb-2 flex flex-wrap items-center justify-between gap-2">
-        <h2 className="text-heading-4-font-size leading-heading-4-line-height">
-          Trending Coins
-        </h2>
-        <Button href="/dashboard/crypto" variant="link" size="small">
-          View More
-        </Button>
-      </header>
-      <TrendingCryptoCoinsTable />
+      <div className="flex h-full flex-col justify-between gap-2">
+        <header className="flex flex-wrap items-center justify-between gap-2">
+          <h2 className="text-heading-4-font-size leading-heading-4-line-height">
+            Trending Coins
+          </h2>
+          <Button href="/dashboard/crypto" variant="link" size="small">
+            View More
+          </Button>
+        </header>
+        <TrendingCryptoCoinsTable />
+      </div>
     </Section>
   )
 }
