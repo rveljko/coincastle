db38482f9be276febe85b609663b274e75860923
Veljko <radisavljevicveljko123@gmail.com>
2025-04-09 09:11:51 +0200

    add light to landing page main

--- src/layouts/landing-page-layout.tsx
+++ src/layouts/landing-page-layout.tsx
@@ -6,10 +6,10 @@ import { Outlet } from 'react-router-dom'
 export default function LandingPageLayout() {
   return (
     <>
-      <Container className="pointer-events-none fixed top-4 bottom-4 left-1/2 -translate-x-1/2">
+      <Container className="pointer-events-none fixed top-4 bottom-4 left-1/2 z-999 -translate-x-1/2">
         <Header />
       </Container>
-      <main className="pt-20.5">
+      <main className="relative pt-20.5 before:absolute before:inset-x-0 before:top-0 before:-z-1 before:h-50 before:bg-(image:--light-gradient)">
         <Outlet />
       </main>
       <Footer />
