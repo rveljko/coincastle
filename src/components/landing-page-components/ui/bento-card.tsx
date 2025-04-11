b97e100642fe26ba06bc6a2f5edc4e17c761c266
Veljko <radisavljevicveljko123@gmail.com>
2025-04-11 09:03:55 +0200

    refactor bento card stylings

--- src/components/landing-page-components/ui/bento-card.tsx
+++ src/components/landing-page-components/ui/bento-card.tsx
@@ -12,7 +12,7 @@ export default function BentoCard({
   return (
     <article
       className={cn(
-        'border-section-outline relative flex w-full flex-col justify-between gap-2 overflow-hidden rounded-2xl border p-4 after:absolute after:inset-0 after:-z-1 after:bg-(image:--bento-card-background)',
+        'border-section-outline flex w-full flex-col justify-between gap-2 rounded-2xl border bg-(image:--bento-card-background) p-4',
         className
       )}
       {...props}
