49032a0278a06b19d82df65914882a04116759a5
Veljko <radisavljevicveljko123@gmail.com>
2025-04-16 11:50:11 +0200

    remove row number in trending crypto coins table skeleton

--- src/components/dashboard-components/trending-crypto-coins-table-skeleton.tsx
+++ src/components/dashboard-components/trending-crypto-coins-table-skeleton.tsx
@@ -12,9 +12,6 @@ export default function TrendingCryptoCoinsTableSkeleton({
       <Table.Header>
         <Table.HeaderRow>
           <Table.HeaderCell>
-            <div className="bg-clickable/10 size-5.5 animate-pulse rounded-sm"></div>
-          </Table.HeaderCell>
-          <Table.HeaderCell>
             <div className="bg-clickable/10 h-5.5 w-20 animate-pulse rounded-sm"></div>
           </Table.HeaderCell>
           <Table.HeaderCell>
@@ -26,9 +23,6 @@ export default function TrendingCryptoCoinsTableSkeleton({
         {Array.from({ length: numberOfCoins }, (_, index) => index).map(
           (row) => (
             <Table.BodyRow key={row} className="hover:bg-unset">
-              <Table.BodyCell>
-                <div className="bg-clickable/10 size-5.5 animate-pulse rounded-sm"></div>
-              </Table.BodyCell>
               <Table.BodyCell>
                 <div className="flex gap-1">
                   <div className="bg-clickable/10 size-5.5 animate-pulse rounded-sm"></div>
