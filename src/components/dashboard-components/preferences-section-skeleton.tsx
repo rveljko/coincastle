f8c0eb68b7650534e17060e80ad44496243a6a56
Veljko <radisavljevicveljko123@gmail.com>
2025-04-18 15:49:09 +0200

    fix preferences section skeleton

--- src/components/dashboard-components/preferences-section-skeleton.tsx
+++ src/components/dashboard-components/preferences-section-skeleton.tsx
@@ -8,7 +8,9 @@ export default function PreferencesSectionSkeleton() {
       <div className="mb-4 space-y-2">
         {themes.map((_, index) => (
           <div key={index}>
-            <div className="bg-clickable/10 mb-1 aspect-[7/5] w-39.5 animate-pulse rounded-sm"></div>
+            <div className="bg-clickable/10 mb-1 w-40 animate-pulse rounded-sm p-4">
+              <div className="aspect-[16/10] size-full"></div>
+            </div>
             <div className="bg-clickable/10 h-5 w-20 animate-pulse rounded-sm"></div>
           </div>
         ))}
@@ -17,7 +19,9 @@ export default function PreferencesSectionSkeleton() {
       <div className="mb-4 space-y-2">
         {charts.map((_, index) => (
           <div key={index}>
-            <div className="bg-clickable/10 mb-1 aspect-[7/5] w-39.5 animate-pulse rounded-sm"></div>
+            <div className="bg-clickable/10 mb-1 w-40 animate-pulse rounded-sm p-4">
+              <div className="aspect-[16/10] size-full"></div>
+            </div>
             <div className="bg-clickable/10 h-5 w-20 animate-pulse rounded-sm"></div>
           </div>
         ))}
