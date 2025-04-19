67daa38540d44bdeec235d534150e5b1a8730f92
Veljko <radisavljevicveljko123@gmail.com>
2025-04-19 13:15:28 +0200

    add key prop to dashboard outlet

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -1,15 +1,17 @@
 import Container from '@components/container'
 import Sidebar from '@components/dashboard-components/sidebar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 export default function DashboardLayout() {
+  const { key } = useLocation()
+
   return (
     <div className="flex flex-row">
       <Sidebar />
       <main className="w-full py-4">
         <Container className="flex h-full flex-col gap-4">
-          <Outlet />
+          <Outlet key={key} />
         </Container>
       </main>
       <ToastContainer />
