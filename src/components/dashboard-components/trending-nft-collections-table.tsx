acda0490f75265f9ce87ad710b77ee663b392e4b
Veljko <radisavljevicveljko123@gmail.com>
2025-09-10 07:49:27 +0200

    update trending nft collections table to moralis api

--- src/components/dashboard-components/trending-nft-collections-table.tsx
+++ src/components/dashboard-components/trending-nft-collections-table.tsx
@@ -1,24 +1,18 @@
 import TrendingNftCollectionsTableSkeleton from '@components/dashboard-components/trending-nft-collections-table-skeleton'
 import ErrorMessage from '@components/dashboard-components/ui/error-message'
 import Table from '@components/dashboard-components/ui/table'
-import VerifiedBadge from '@components/dashboard-components/ui/verified-badge'
 import { trendingNftCollectionsTableHeaders } from '@data/table-headers'
 import useGetNftCollections from '@hooks/queries/use-get-nft-collections'
 import { ethereumPriceFormatter } from '@utils/helpers/currency-formatter'
-import isHttpError from '@utils/helpers/is-http-error'
 import { Link } from 'react-router-dom'
 
 export default function TrendingNftCollectionsTable() {
-  const { data, isPending, error } = useGetNftCollections(
-    'volume_total',
-    'desc',
-    5
-  )
+  const { data, isPending, error } = useGetNftCollections()
 
   if (isPending)
     return <TrendingNftCollectionsTableSkeleton numberOfNftCollections={5} />
 
-  if (error || isHttpError(data.code)) return <ErrorMessage />
+  if (error) return <ErrorMessage />
 
   return (
     <Table>
@@ -30,38 +24,37 @@ export default function TrendingNftCollectionsTable() {
         </Table.HeaderRow>
       </Table.Header>
       <Table.Body>
-        {data.data.map(
-          ({
-            contract_address,
-            name,
-            logo_url,
-            opensea_verified,
-            floor_price,
-          }) => (
-            <Table.BodyRow key={contract_address} className="relative">
-              <Table.BodyCell>
-                <Link to={`/dashboard/collection/${contract_address}`}>
-                  <span className="absolute inset-0"></span>
-                  <div className="flex w-max items-center gap-1">
-                    <img
-                      className="size-5.5 rounded-full"
-                      src={logo_url}
-                      title={name}
-                      alt={name}
-                      loading="lazy"
-                    />
-                    <div className="flex items-center gap-0.5">
-                      {name} {opensea_verified && <VerifiedBadge isSmall />}
+        {data
+          .slice(0, 5)
+          .map(
+            ({
+              collection_address,
+              collection_image,
+              collection_title,
+              floor_price,
+            }) => (
+              <Table.BodyRow key={collection_address} className="relative">
+                <Table.BodyCell>
+                  <Link to={`/dashboard/collection/${collection_address}`}>
+                    <span className="absolute inset-0"></span>
+                    <div className="flex w-max items-center gap-1">
+                      <img
+                        className="size-5.5 rounded-full"
+                        src={collection_image}
+                        title={collection_title}
+                        alt={collection_title}
+                        loading="lazy"
+                      />
+                      {collection_title}
                     </div>
-                  </div>
-                </Link>
-              </Table.BodyCell>
-              <Table.BodyCell>
-                {ethereumPriceFormatter(floor_price)}
-              </Table.BodyCell>
-            </Table.BodyRow>
-          )
-        )}
+                  </Link>
+                </Table.BodyCell>
+                <Table.BodyCell>
+                  {ethereumPriceFormatter(floor_price)}
+                </Table.BodyCell>
+              </Table.BodyRow>
+            )
+          )}
       </Table.Body>
     </Table>
   )
